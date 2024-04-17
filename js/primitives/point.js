bdf084bc5e9083c15bd05fac53b97f55d8445e84
penteler <impromooray@gmail.com>
2024-04-17 16:55:13 +0530

    selecting points

--- js/primitives/point.js
+++ js/primitives/point.js
@@ -9,11 +9,18 @@ class Point {
     }
 
 
-    draw(ctx, size = 18, color = "black"){
+    draw(ctx, { size = 18, color = "black", outline = false } ={}){
         const rad = size / 2;
         ctx.beginPath();
         ctx.fillStyle = color;
         ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
         ctx.fill();
+        if (outline) {
+          ctx.beginPath();
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = "yellow";
+          ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
+          ctx.stroke()
+        }
     }
 }
